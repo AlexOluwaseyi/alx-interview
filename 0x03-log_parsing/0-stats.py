8b6e3061418e694dfa19859447a318a663181942
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2024-05-02 16:03:27 -0700

    Finished

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,6 +1,22 @@
 #!/usr/bin/python3
 
 """
+script that reads stdin line by line and computes metrics:
+
+- Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
+  <status code> <file size>
+  (if the format is not this one, the line must be skipped)
+- After every 10 lines and/or a keyboard interruption (CTRL + C),
+  print these statistics from the beginning:
+- Total file size: File size: <total size>
+- where <total size> is the sum of all previous <file size>
+  (see input format above)
+- Number of lines by status code:
+  - possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
+  - if a status code doesn’t appear or is not an integer,
+    don’t print anything for this status code
+  - format: <status code>: <number>
+  - status codes should be printed in ascending order
 """
 
 import re
@@ -9,7 +25,8 @@ import signal
 import sys
 
 
-status_record = dict(sorted({}))
+status_record = {}
+sorted_dict = {}
 file_size = 0
 
 # Regex patterns
@@ -20,19 +37,18 @@ status_code_pattern = r'(\d{3})'
 size_pattern = r'(\d+)$'
 
 
-
-
 def get_essentials(line):
     """
     Extract status_code and file_size from each line
     Performed after line.split() operation
     """
-    # status_code = re.search(status_code_pattern, line).group()
-    # size = int(re.search(size_pattern, line).group())
     global file_size
 
     status_code = line.split()[-2]
     size = int(line.split()[-1])
+
+    if int(status_code) is ValueError:
+        return
 
     if status_code not in status_record.keys():
         status_record[status_code] = 1
@@ -44,26 +60,48 @@ def get_essentials(line):
 
 
 def printer(status_record):
+    """
+    function to print cummulative file_size and
+    sorted dict of status code frequency
+    """
     total_freq = 0
     global file_size
-    # sorted_dict = dict(sorted(status_record.items()))
 
-    for value in status_record.values():
+    for value in sorted_dict.values():
         total_freq += value
     if total_freq % 10 == 0:
         print(f'File size: {file_size}')
-        for key, value in status_record.items():
+        for key, value in sorted_dict.items():
             print(f'{key}: {value}')
 
+
 def signal_handler(signal, frame):
+    """
+    Function to handle KeyboardInterrupt (^C)
+    """
     print(f'File size: {file_size}')
-    for key, value in status_record.items():
+    for key, value in sorted_dict.items():
         print(f'{key}: {value}')
     sys.exit(0)
 
-signal.signal(signal.SIGINT, signal_handler)
+# signal.signal(signal.SIGINT, signal_handler)
 
-for line in fileinput.input():
-    line = line.strip('\n\r')
-    get_essentials(line)
-    printer(status_record)
+
+def execution():
+    """
+    Function to read line from stdin and call other functions
+    """
+    signal.signal(signal.SIGINT, signal_handler)
+
+    global status_record
+    global sorted_dict
+    for line in fileinput.input():
+        line = line.strip('\n\r')
+        get_essentials(line)
+        sorted_dict = dict(sorted(status_record.items()))
+        printer(sorted_dict)
+
+
+if __name__ == "__main__":
+    """Call to execution() function here"""
+    execution()
