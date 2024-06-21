884a4c0eda457216354c536267a55aa17ca91228
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2024-06-21 06:16:03 +0100

    Without sieve of Eratosthenes

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -4,8 +4,6 @@
 """
 
 
-'''
-# Without Sieve of Eratosthenes
 def is_prime(num):
     """Checks if a number is prime."""
     if num <= 1:
@@ -59,59 +57,37 @@ def isWinner(x, nums):
         return 'Ben'
     else:
         return None
+
+
 '''
-
-
-# With Sieve of Eratosthenes
-def sieve(n):
-    is_prime = [True] * (n + 1)
-    p = 2
-    while p * p <= n:
-        if is_prime[p]:
-            for i in range(p * p, n + 1, p):
-                is_prime[i] = False
-        p += 1
-    primes = [p for p in range(2, n + 1) if is_prime[p]]
-    return primes
-
-
-def simulate_game(n):
-    primes = sieve(n)
-    if not primes:
-        return 'Ben'
-
-    moves = 0
-    remaining = set(range(1, n + 1))
-
-    for prime in primes:
-        if prime in remaining:
-            multiples = set(range(prime, n + 1, prime))
-            remaining -= multiples
-            moves += 1
-
-    return 'Maria' if moves % 2 != 0 else 'Ben'
-
-
 def isWinner(x, nums):
-    maria_wins = 0
-    ben_wins = 0
+    maria_score = 0
+    ben_score = 0
 
-    for n in nums:
-        winner = simulate_game(n)
-        if winner == 'Maria':
-            maria_wins += 1
+    for num in nums:
+        primes = [True for i in range(max(nums) + 1)]
+        primes[0] = primes[1] = False
+        prime_count = 0
+
+        for i in range(2, int(len(primes) ** 0.5) + 1):
+            if primes[i]:
+                prime_count += 1
+                for j in range(i*i, len(primes), i):
+                    primes[j] = False
+
+        if prime_count % 2 == 0:
+            ben_score += 1
         else:
-            ben_wins += 1
+            maria_score += 1
 
-    if maria_wins > ben_wins:
+    if maria_score > ben_score:
         return 'Maria'
-    elif ben_wins > maria_wins:
+    elif ben_score > maria_score:
         return 'Ben'
     else:
         return None
 
 
-'''
 def isWinner(x, nums):
     """
     Prototype: def isWinner(x, nums)
@@ -161,63 +137,4 @@ def is_prime(num):
         else:
             return True
     return False
-'''
-
-'''
- def is_prime(num):
-     """Checks if a number is prime."""
-     if num <= 1:
-         return False
-     for i in range(2, int(num**0.5) + 1):
-         if num % i == 0:
-             return False
-     return True
- def simulate_game(n):
-     """Simulate a single game for given n and return the winner."""
-     remaining = list(range(1, n + 1))
-     current_turn = 'Maria'
-     while True:
-         # Log the current state of the list
-         print(f"Current list: {remaining}")
-
-         # Find the first prime in the remaining numbers
-         prime_found = False
-         for num in remaining:
-             if is_prime(num):
-                 prime = num
-                 prime_found = True
-                 break
-         if not prime_found:
-             # No prime found means current player loses
-             return 'Ben' if current_turn == 'Maria' else 'Maria'
-         # Logging the current move
-         print(f"{current_turn} picks {prime}")
-         # Remove the prime and its multiples
-         removed = [num for num in remaining if num % prime == 0]
-         remaining = [num for num in remaining if num % prime != 0]
-         # Logging the removed numbers
-         print(f"Removed numbers: {removed}")
-         # Switch turn
-         current_turn = 'Ben' if current_turn == 'Maria' else 'Maria'
- def isWinner(x, nums):
-     """Determine the player that won the most rounds."""
-     maria_wins = 0
-     ben_wins = 0
-     for n in nums:
-         print(f"Starting game with n = {n}")
-         winner = simulate_game(n)
-         print(f"Winner of this round: {winner}\n")
-         if winner == 'Maria':
-             maria_wins += 1
-         else:
-             ben_wins += 1
-     if maria_wins > ben_wins:
-         return 'Maria'
-     elif ben_wins > maria_wins:
-         return 'Ben'
-     else:
-         return None
- # Test match where Maria wins
- if __name__ == "__main__":
-     print("Overall Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
 '''
